311800b38c76f834f6b187e706e94eb216c93ea7
Kacper Kazimierak <kacperkazimierak@gmail.com>
2020-06-17 12:20:53 +0200

    Added exported functions docs

--- sluggo.go
+++ sluggo.go
@@ -6,41 +6,41 @@ import (
 )
 
 type SlugOptions struct {
-	TrimSpaces       bool
+	TrimSpaces bool
 	// If true spaces will be trimmed from both ends of the input text, default true.
 	// If PreserveLength set to true - spaces will not be trimmed
 
-	Case             int
+	Case int
 	// Case conversion: sluggo.DefaultCase = 0, sluggo.Lowercase = 1, sluggo.Uppercase = 2, default Lowercase
 
 	SpaceSymbol string
 	// Symbol for replacing space. URL safe symbols are; a-z A-Z 0-9 "." "-" "_" "~", default _
 
-	CharSymbol       string
+	CharSymbol string
 	// Symbol for replacing unwanted chars.  URL safe symbols are; a-z A-Z 0-9 "." "-" "_" "~", default -
 
-	MaxLength        int
+	MaxLength int
 	// Trim text to this length
 
 	PreserveLength bool
-    // By default neighbouring chars to replace will be replaced with 1 CharSymbol or SpaceSymbol,
-    //for example: "foo  b@@@r" => "foo_b-r". If true input text and output text will be the same length,
-    //for example (with default SpaceSymbol and CharSymbol): "foo  bar" => "foo__bar" instead of "foo_bar",
-    //"foo b@@r" => "foo_b--r" instead of "foo_b-r"
+	// By default neighbouring chars to replace will be replaced with 1 CharSymbol or SpaceSymbol,
+	//for example: "foo  b@@@r" => "foo_b-r". If true input text and output text will be the same length,
+	//for example (with default SpaceSymbol and CharSymbol): "foo  bar" => "foo__bar" instead of "foo_bar",
+	//"foo b@@r" => "foo_b--r" instead of "foo_b-r"
 }
 
 const (
 	DefaultCase = 0
-	Lowercase = 1
-	Uppercase = 2
+	Lowercase   = 1
+	Uppercase   = 2
 )
 
 var (
 	allowedCharsRegexp = regexp.MustCompile("[^a-z0-9-_~.]+")
-	whitespaceRegexp = regexp.MustCompile("\\s+")
+	whitespaceRegexp   = regexp.MustCompile("\\s+")
 
 	preserveAllowedCharsRegexp = regexp.MustCompile("[^a-z0-9-_~.]")
-	preserveWhitespaceRegexp = regexp.MustCompile("\\s")
+	preserveWhitespaceRegexp   = regexp.MustCompile("\\s")
 )
 
 func NewDefaultOptions() *SlugOptions {
@@ -54,15 +54,43 @@ func NewDefaultOptions() *SlugOptions {
 	}
 }
 
+// Returns slug from supplied text using default options:
+// - TrimSpaces:     true,
+// - MaxLength:      0,
+// - Case:           Lowercase,
+// - SpaceSymbol:    "_",
+// - CharSymbol:     "-",
+// - PreserveLength: false,
 func GetSlug(text string) string {
 	// Run GetSlugWithOpts with default options set
 	return GetSlugWithOpts(text, NewDefaultOptions())
 }
 
+// Return slug created using provided parameters:
+//TrimSpaces bool
+// If true spaces will be trimmed from both ends of the input text, default true.
+// If PreserveLength set to true - spaces will not be trimmed
+
+//Case int
+// Case conversion: sluggo.DefaultCase = 0, sluggo.Lowercase = 1, sluggo.Uppercase = 2, default Lowercase
+
+//SpaceSymbol string
+// Symbol for replacing space. URL safe symbols are; a-z A-Z 0-9 "." "-" "_" "~", default _
+
+//CharSymbol string
+// Symbol for replacing unwanted chars.  URL safe symbols are; a-z A-Z 0-9 "." "-" "_" "~", default -
+
+//MaxLength int
+// Trim text to this length
+
+//PreserveLength bool
+// By default neighbouring chars to replace will be replaced with 1 CharSymbol or SpaceSymbol,
+//for example: "foo  b@@@r" => "foo_b-r". If true input text and output text will be the same length,
+//for example (with default SpaceSymbol and CharSymbol): "foo  bar" => "foo__bar" instead of "foo_bar",
+//"foo b@@r" => "foo_b--r" instead of "foo_b-r"
 func GetSlugWithOpts(text string, opts *SlugOptions) string {
 
 	var spaceRegexp, charsRegexp *regexp.Regexp
-
 
 	// Trim trailing and leading spaces
 	if opts.TrimSpaces && !opts.PreserveLength {
---
+++ sluggo_test.go
@@ -0,0 +1,75 @@
+package sluggo
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetSlug(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "unchanged", args: args{text:"foo"}, want: "foo"},
+		{name: "to mixed case", args: args{text:"FoO"}, want: "foo"},
+		{name: "with space", args: args{text:"foo bar"}, want: "foo_bar"},
+		{name: "multiple spaces", args: args{text:"f  oo"}, want: "f_oo"},
+		{name: "Special characters", args: args{text:"f*$oo&śŚb@r"}, want: "f-oo-b-r"},
+		{name: "With dash", args: args{text:"foo-bar"}, want: "foo-bar"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {Szekspira 1
+			if got := GetSlug(tt.args.text); got != tt.want {
+				t.Errorf("GetSlug() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetSlugWithOpts(t *testing.T) {
+	type args struct {
+		text string
+		opts *SlugOptions
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetSlugWithOpts(tt.args.text, tt.args.opts); got != tt.want {
+				t.Errorf("GetSlugWithOpts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewDefaultOptions(t *testing.T) {
+	tests := []struct {
+		name string
+		want *SlugOptions
+	}{
+		{name: "test defaults", want: &SlugOptions{
+			TrimSpaces:     true,
+			MaxLength:      0,
+			Case:           Lowercase,
+			SpaceSymbol:    "_",
+			CharSymbol:     "-",
+			PreserveLength: false,
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewDefaultOptions(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewDefaultOptions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
